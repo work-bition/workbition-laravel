43fb897bedf597422280c3153b4ae6a2cc04a08b
work-bition <work-bition@outlook.com>
2019-04-22 23:28:26 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Notifications\TopicReplied;
 use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
@@ -22,6 +23,9 @@ class ReplyObserver
         $reply->topic->reply_count = $reply->topic->replies->count();
 
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
 
     }
 
