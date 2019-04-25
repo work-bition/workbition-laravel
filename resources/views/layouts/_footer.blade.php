8fd4ce19a6bf2ff2399d413813745c14711aa9bd
work-bition <work-bition@outlook.com>
2019-04-25 16:27:30 +0800

    站点设置

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    .
+    .
+    .
+
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
+  </div>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,14 @@
 <html lang="{{ app()->getLocale() }}">
 
   <head>
-    <title>@yield('title', '优拾课堂 · 办公与数字生活指南')</title>
+    <title>@yield('title', '优拾课堂 · 办公与数字生活指南') - {{ setting('site_name', '优涵课堂')}}</title>
     <link rel="stylesheet" href="{{ mix('css/main.css') }}">
 
     @yield('styles')
 
     <meta charset="utf-8">
-    <meta name="description" content="@yield('description', '办公与数字生活指南')" />
+    <meta name="description" content="@yield('description', setting('seo_description', '办公与数字生活指南'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', '职场,提升,工作,技能,办公,office, word,excel,ppt'))" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0,viewport-fit=cover">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
