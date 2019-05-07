541e7bf79ba40750d30802990e33a98c4d65b669
work-bition <work-bition@outlook.com>
2019-05-07 11:01:37 +0800

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -8,56 +8,79 @@ use App\Http\Requests\Api\VerificationCodeRequest;
 
 class VerificationCodesController extends Controller
 {
+
     //增加了 store 方法，利用 DingoApi 的 Helpers trait，我们可以使用 $this->response->array 返回一个测试用的响应。
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
 
-      $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
 
-      if (!app()->environment('production')) {
+        if (!$captchaData) {
 
-        $code = '1234';
-      }
-
-      else {
-
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
-
-        try {
-
-          $result = $easySms->send($phone, [
-
-              'content'  =>  "【dd】您的验证码是{$code}。如非本人操作，请忽略本短信"
-
-          ]);
+            return $this->response->error('图片验证码已失效', 422);
 
         }
 
-        catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
 
-          $message = $exception->getException('yunpian')->getMessage();
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
 
-          return $this->response->errorInternal($message ?: '短信发送异常');
+            return $this->response->errorUnauthorized('验证码错误');
 
         }
 
-      }
+        $phone = $captchaData['phone'];
 
-      $key = 'verificationCode_'.str_random(15);
+        if (!app()->environment('production')) {
 
-      $expiredAt = now()->addMinutes(10);
+            $code = '1234';
 
-      // 缓存验证码 10分钟过期。
-      \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        }
 
-      return $this->response->array([
+        else {
 
-        'key' => $key,
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
-        'expired_at' => $expiredAt->toDateTimeString(),
+            try {
 
-      ])->setStatusCode(201);
+                $result = $easySms->send($phone, [
+
+                    'content'  =>  "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+
+                ]);
+
+            }
+
+            catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+
+                $message = $exception->getException('yunpian')->getMessage();
+
+                return $this->response->errorInternal($message ?? '短信发送异常');
+
+            }
+
+        }
+
+        $key = 'verificationCode_'.str_random(15);
+
+        $expiredAt = now()->addMinutes(10);
+
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
+
+        return $this->response->array([
+
+            'key' => $key,
+
+            'expired_at' => $expiredAt->toDateTimeString(),
+
+        ])->setStatusCode(201);
 
     }
+    
 }
